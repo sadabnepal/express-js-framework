7cbfe22cdac10e9d41a47d493019cd855d1a6c64
MD SADAB SAQIB <sadabnepal1993@gmail.com>
2022-01-05 12:40:48 +0530

    Update constants.js

--- static/constants.js
+++ static/constants.js
@@ -1,4 +1,4 @@
-export const RECORD_CREATED_MESSAGE = "record created!!"
-export const RECORD_UPDATED_MESSAGE = "record updated!!"
+export const RECORD_CREATED_MESSAGE = "Record created!!"
+export const RECORD_UPDATED_MESSAGE = "Record updated!!"
 export const RECORD_DELETED_MESSAGE = (id) => `User with ID ${id} record deleted!!!`
 export const WELCOME_MESSAGE = "Welcome to ExpressJS Homepage!!!"
